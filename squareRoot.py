35a1c431a82d8449f0cdb4f4beb51b3ce6314c11
PROFESSOR <omair2019@namal.edu.pk>
2021-10-03 12:21:56 +0500

    sqrt fun

---
+++ squareRoot.py
@@ -0,0 +1,21 @@
+def sub (x,y):
+    return x-y
+a=sub(8,14)
+print(a)
+
+def divide (x,y):
+	return x / y
+
+print(12,7)
+
+#python addition
+def add(x,y):
+	return (x+y)
+m=add(6,9)
+print(m)
+
+def squareRoot(x):
+	return x ** 0.5
+
+x = squareRoot(10)
+print(x)
